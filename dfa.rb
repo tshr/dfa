090c4f4b9e7c253d4f0ebac8cf5b3e12b721b65a
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-07-09 08:46:28 -0700

    changed delete ifs to sekects made get_state_by_name a private method, some code style tweaks

--- dfa.rb
+++ dfa.rb
@@ -6,23 +6,26 @@ class DFA
 
   def initialize(states, transition_hash)
     @states = states
-    @initial_state = states.map{|state| state if state.is_start}.delete_if {|x| x == nil}.first
+    @initial_state = states.select{|s| s.is_start}.first
     @transition_hash = transition_hash
-  end
-
-  def get_state_by_name(name)
-    @states.map{|state| state if state.name == name}.delete_if {|x| x == nil}.first
   end
 
   def accepts?(string)
 
     current_state = @initial_state
-    string.each_char do |char|
-      current_state = get_state_by_name(@transition_hash[current_state.name][char])
+    string.each_char do |ch|
+      current_state = get_state_by_name(@transition_hash[current_state.name][ch])
     end
 
     !!current_state.is_accept
   end
+
+  private
+
+  def get_state_by_name(name)
+    @states.select{|s| s.name == name}.first
+  end
+
 end
 
 # @@test_transition_hash = {
@@ -33,3 +36,8 @@ end
 # }
 
 # @@test_dfa = DFA.new([State.new("s1", true, true), State.new("s2", false, false)], @@test_transition_hash)
+
+# maybe use inject instead of each_char
+# validations
+# final state method
+# tests
