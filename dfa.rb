c2aee0e36ab63b41c7e4748cf3de6ab75bd86758
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-07-12 13:38:04 -0700

    added some exception handling to private methods, commented test hash

--- dfa.rb
+++ dfa.rb
@@ -5,9 +5,11 @@ class DFA
   attr_reader :states, :initial_state, :transition_hash
 
   def initialize(states, transition_hash)
+
     @states = states
     @initial_state = states.select{|s| s.is_start}.first
     @transition_hash = transition_hash
+
   end
 
   def accept_states
@@ -25,11 +27,24 @@ class DFA
   private
 
   def get_state_by_name(name)
-    @states.select{|s| s.name == name}.first
+    state = @states.select{|s| s.name == name}.first
+    raise "State with name #{name} could not be found" unless state
+    state
   end
 
   def compute_next_state(current_state, character)
-    get_state_by_name @transition_hash[current_state.name][character]
+    if @transition_hash[current_state.name] && (next_state_name = @transition_hash[current_state.name][character])
+      get_state_by_name next_state_name
+    else
+      raise "Could not calculate the next state's name with inputs -- state: #{current_state} and character: #{character}"
+    end
   end
 
 end
+
+# test_transition_hash = {
+#   "s1" => {"0" => "s1", "1" => "s2"},
+#   "s2" => {"0" => "s2", "1" => "s1"}
+# }
+
+# @test_dfa = DFA.new([State.new("s1", "true", "true"), State.new("s2", "false", "false")], test_transition_hash)
