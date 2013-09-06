caa193c663b01ea36bea570cbb028434a887d3d6
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-09-05 20:08:17 -0700

    moar examples

--- dfa.rb
+++ dfa.rb
@@ -10,12 +10,16 @@
          "s2" => {"0" => "s2", "1" => "s1"}
   }
 
-  DFA.new([State.new("s1", true, true), State.new("s2", false, false)], transition_hash)
+  foo = DFA.new([State.new("s1", true, true), State.new("s2", false, false)], transition_hash)
 
   This creates a DFA composed of two states (s1, and s2) where s1 is both the start state
   and the only accept state. The DFA's alphabet is {0,1} and 1's cause the state to change
-  This matches the language of strings of 0 and 1 where there are an even number of 1's.
-  
+  This matches the language of strings of 0 and 1 where there are an even number of 1's. E.g.
+
+  foo.accepts?("1010101") # => true
+  foo.accepts?("101010") # => false
+  foo.compute_final_state("1010101") # => #<struct State name="s1", is_start=true, is_accept=true>
+
 =end
 
 State = Struct.new(:name, :is_start, :is_accept)
